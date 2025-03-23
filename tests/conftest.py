31f9c4d531f73d0872220462680b49efbb8c78a6
Rostislav Ivanov <x1qeee25@gmail.com>
2025-03-23 22:00:44 +0300

    Информация для запуска автотестов в дженкинсе

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,15 @@
+import os
 import pytest
 from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from utils import attachment
+from dotenv import load_dotenv
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_env():
+    load_dotenv()
 
 
 @pytest.fixture(scope='function', autouse=True)
@@ -7,8 +17,34 @@ def setup_browser():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     browser.config.base_url = 'https://yasno.live'
-    #browser.config.timeout = 10000
+    browser.config.timeout = 10000
+
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "127.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+
+    SELENOID_LOGIN = os.getenv("SELENOID_LOGIN")
+    SELENOID_PASSWORD = os.getenv("SELENOID_PASSWORD")
+    SELENOID_URL = os.getenv("SELENOID_URL")
+
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://{SELENOID_LOGIN}:{SELENOID_PASSWORD}@{SELENOID_URL}/wd/hub",
+        options=options
+    )
+    browser.config.driver = driver
 
     yield
+
+    attachment.add_screenshot(browser)
+    attachment.add_logs(browser)
+    attachment.add_html(browser)
+    attachment.add_video(browser)
 
     browser.quit()
--- tests/therapists_test.py
+++ tests/therapists_test.py
@@ -1,35 +1,68 @@
+import allure
+from allure_commons.types import Severity
+
 from model.pages.therapists_page import therapists
 
 
 class TestTherapistsPage:
+
+    @allure.label('owner', 'Ivanov Rostislav')
+    @allure.feature('Проверка работы страницы therapists')
+    @allure.tag('web')
+    @allure.severity(Severity.NORMAL)
     def test_filters_should_has_short_filters(self):
         (therapists
          .open()
          .filters_should_be_not_selected())
 
+    @allure.label('owner', 'Ivanov Rostislav')
+    @allure.feature('Проверка работы страницы therapists')
+    @allure.tag('web')
+    @allure.severity(Severity.CRITICAL)
     def test_header_links_should_be_clickable(self):
         (therapists.open()
          .links_should_be_clickable_header())
 
+    @allure.label('owner', 'Ivanov Rostislav')
+    @allure.feature('Проверка работы страницы therapists')
+    @allure.tag('web')
+    @allure.severity(Severity.BLOCKER)
     def test_psychologist_cards_should_increase(self):
         (therapists.open()
          .therapists_cards_should_increase())
+
+    @allure.label('owner', 'Ivanov Rostislav')
+    @allure.feature('Проверка работы страницы therapists')
+    @allure.tag('web')
+    @allure.severity(Severity.CRITICAL)
     def test_sorting_for_max_price(self):
         (therapists.open()
          .choose_max_filter()
          .check_max_price_in_card())
 
+    @allure.label('owner', 'Ivanov Rostislav')
+    @allure.feature('Проверка работы страницы therapists')
+    @allure.tag('web')
+    @allure.severity(Severity.CRITICAL)
     def test_compare_price_popup_and_card(self):
         (therapists.open()
          .click_info_icon_in_card()
          .check_price_in_info_popup())
 
+    @allure.label('owner', 'Ivanov Rostislav')
+    @allure.feature('Проверка работы страницы therapists')
+    @allure.tag('web')
+    @allure.severity(Severity.NORMAL)
     def test_choose_couple_therapy(self):
         (therapists.open()
          .choose_couple_therapy()
          .check_couple_symptoms_in_popup()
          .check_couple_therapy_short_filters())
 
+    @allure.label('owner', 'Ivanov Rostislav')
+    @allure.feature('Проверка работы страницы therapists')
+    @allure.tag('web')
+    @allure.severity(Severity.MINOR)
     def test_footer_should_have_links(self):
         (therapists.open()
          .check_footer_page_for_links())
